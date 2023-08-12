73a19e477355c3a3b31e6b4ac0de28c5841a1073
HaberSerhii <127188736+HaberSerhii@users.noreply.github.com>
2023-08-12 10:18:35 +0000

    Deploying to gh-pages from @ HaberSerhii/parcel-practice@d93a809e843a145919d0a193914739cd79e0b401 🚀

---
+++ index.6d0569c4.js
@@ -0,0 +1,2 @@
+const e=document.querySelector('link[media="(prefers-color-scheme: light)"]');console.log(e);
+//# sourceMappingURL=index.6d0569c4.js.map
---
+++ index.a210307b.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector('link[media="(prefers-color-scheme: light)"]');console.log(e)}();
+//# sourceMappingURL=index.a210307b.js.map
