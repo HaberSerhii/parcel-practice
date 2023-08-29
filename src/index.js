d7f547ea0c7014e9178b30c81440c32371954748
Serhii Haber <haberserhii@icloud.com>
2023-08-29 12:51:57 +0300

    upd

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
 const lightThemeLink = document.querySelector(
   'link[media="(prefers-color-scheme: light)"]'
 );
+const darkThemeLink = document.querySelector(
+  'link[media="(prefers-color-scheme: dark)"]'
+);
+document.addEventListener('DOMContentLoaded', function () {
+  const switcherControls = document.querySelectorAll('.switcher-control');
+  const body = document.body;
 
-console.log(lightThemeLink);
+  switcherControls.forEach(function (switcher) {
+    switcher.addEventListener('change', function () {
+      const selectedTheme = switcher.getAttribute('data-theme');
+
+      // Удаляем все классы связанные с темами
+      body.classList.remove('light-theme', 'dark-theme');
+
+      // Добавляем выбранный класс темы
+      body.classList.add(selectedTheme);
+    });
+  });
+});
